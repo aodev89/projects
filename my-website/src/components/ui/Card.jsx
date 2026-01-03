3f7f292150ce0d08d21b477ffbd7b41449dc9dae
aodev89 <exalcius@gmail.com>
2026-01-03 20:18:03 +0100

    changes had to be done

--- my-website/src/components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children }) {
-    return (
-        <div className="rounded-lg border border-border p-6 hover:shadow-sm transition">
-            {children}
-        </div>
-    )
-}
